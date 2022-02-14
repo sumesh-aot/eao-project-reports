13c27bec51a4a4791ba2c12ccfeae72b64e76cc1
Sumesh Punakkal Kariyil <sumesh.pk@aot-technologies.com>
2022-02-14 15:27:47 -0800

    Draft project setup

---
+++ src/reports_api/resources/__init__.py
@@ -0,0 +1,60 @@
+# Copyright © 2019 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Exposes all of the resource endpoints mounted in Flask-Blueprint style.
+
+Uses restplus namespaces to mount individual api endpoints into the service.
+
+All services have 2 defaults sets of endpoints:
+ - ops
+ - meta
+That are used to expose operational health information about the service, and meta information.
+"""
+from flask import Blueprint
+
+from .code import API as CODES_API
+from .ops import API as OPS_API
+from .apihelper import Api
+from .meta import API as META_API
+
+__all__ = ('API_BLUEPRINT', 'OPS_BLUEPRINT')
+
+# This will add the Authorize button to the swagger docs
+AUTHORIZATIONS = {'apikey': {'type': 'apiKey', 'in': 'header', 'name': 'Authorization'}}
+
+OPS_BLUEPRINT = Blueprint('API_OPS', __name__, url_prefix='/ops')
+
+API_OPS = Api(
+    OPS_BLUEPRINT,
+    title='Service OPS API',
+    version='1.0',
+    description='The Core API for the Reports System',
+    security=['apikey'],
+    authorizations=AUTHORIZATIONS,
+)
+
+API_OPS.add_namespace(OPS_API, path='/')
+
+API_BLUEPRINT = Blueprint('API', __name__, url_prefix='/api/v1')
+
+API = Api(
+    API_BLUEPRINT,
+    title='EAO Reports API',
+    version='1.0',
+    description='The Core API for the Reports System',
+    security=['apikey'],
+    authorizations=AUTHORIZATIONS,
+)
+
+API.add_namespace(META_API, path='/meta')
+API.add_namespace(CODES_API, path='/codes')
---
+++ src/reports_api/services/code.py
@@ -0,0 +1,55 @@
+# Copyright © 2019 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Service to manage Fee Calculation."""
+
+from flask import current_app
+
+from reports_api.models import db, CodeTable
+
+
+class CodeService:
+    """Service to manage Fee related operations."""
+
+    @classmethod
+    def find_code_values_by_type(
+            cls,
+            code_type: str
+    ):
+        """Find code values by code type."""
+        current_app.logger.debug(f'<find_code_values_by_type : {code_type}')
+        model: CodeTable = cls._find_model_from_type(code_type)
+        response = {'codes':[]}
+        for row in model.find_all():
+            response['codes'].append(row.as_dict())
+
+        current_app.logger.debug('>find_code_values_by_type')
+        return response
+
+    @classmethod
+    def _find_model_from_type(cls, code_type):
+        for model_class in db.Model._decl_class_registry.values():  # pylint:disable=protected-access
+            if hasattr(model_class, '__tablename__') and model_class.__tablename__ == code_type:
+                return model_class
+
+    @classmethod
+    def find_code_value_by_type_and_code(
+            cls,
+            code_type: str,
+            code: str
+    ):
+        """Find code values by code type and code."""
+        current_app.logger.debug(f'<find_code_value_by_type_and_code : {code_type} - {code}')
+        model: CodeTable = cls._find_model_from_type(code_type)
+        current_app.logger.debug('>find_code_value_by_type_and_code')
+        return model.as_dict()
